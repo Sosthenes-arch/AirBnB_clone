4561d27acbe477223cda1b5c83ea9deaac0f0b1e
Sosthenes-arch <usosthenes@gmail.com>
2023-05-14 14:10:02 -0700

    AirBnB project

--- models/state.py
+++ models/state.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
-"""State class."""
+"""
+Defines the State class
+"""
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """Represent a state.
+    """Represent a state
+
     Attributes:
-        name (str): The name of the state.
+        name (str): The name of the state
+
     """
     name = ""
